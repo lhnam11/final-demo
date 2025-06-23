66dcaf25a3aa42812d6a589f1efdc74a72459efb
Lê Hoài Nam <nvhoainam126@gmail.com>
2025-06-23 17:11:43 +0700

    t

--- src/main/java/com/nam/qa/POM/BaseClass.java
+++ src/main/java/com/nam/qa/POM/BaseClass.java
@@ -2,6 +2,7 @@ package com.nam.qa.POM;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
@@ -16,9 +17,19 @@ public class BaseClass {
 //		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 //		driver.manage().window().maximize();
 
+//		System.setProperty("webdriver.edge.driver", "D:\\LeHoaiNam\\Selenium\\msedgedriver.exe");
+//
+//		driver = new EdgeDriver();
+//		driver.manage().window().maximize();
+		
 		System.setProperty("webdriver.edge.driver", "D:\\LeHoaiNam\\Selenium\\msedgedriver.exe");
 
-		driver = new EdgeDriver();
+		EdgeOptions options = new EdgeOptions();
+		options.addArguments("--headless");        // chạy không giao diện (cho Jenkins)
+		options.addArguments("--disable-gpu");     // bắt buộc cho Windows
+		options.addArguments("--window-size=1920,1080"); // giúp layout đúng
+
+		driver = new EdgeDriver(options);
 		driver.manage().window().maximize();
 	}
 
